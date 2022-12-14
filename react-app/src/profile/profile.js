9798e67da62cb22d7190dc4bb17fe8da6d23b94b
obreese <breese.o@northeastern.edu>
2022-12-14 01:57:44 -0500

    posts working everywhere

--- react-app/src/profile/profile.js
+++ react-app/src/profile/profile.js
@@ -2,25 +2,18 @@ import React from "react";
 import {useLocation} from "react-router-dom";
 import PublicProfileComponent from "./public-profile";
 import EditableProfileComponent from "./editable-profile";
-import {useSelector, useDispatch} from "react-redux";
-import {findUserByIdThunk} from "../user/user-thunk";
+import {useSelector} from "react-redux";
 
 const ProfileComponent = ({myProfile}) => {
-    console.log("Begin profileComponent")
-    const {currentUser, profile} = useSelector((state) => state.user)
+    const {currentUser } = useSelector((state) => state.user)
     let viewedId = '';
-    //let viewedProfile = null
-    const dispatch = useDispatch()
 
     let location = useLocation().pathname.split("/")
     if (location.length === 2 || (location.length === 3 && location[2] === '')) {
-            viewedId = '' + currentUser._id;
-            //viewedProfile = currentUser
+        viewedId = '' + currentUser._id;
     }
     else {
         viewedId = location[2]
-        //dispatch(findUserByIdThunk(location[2]))
-        //viewedProfile = profile
     }
 
     if ((currentUser && myProfile) || (currentUser && currentUser.isAdmin)) {
